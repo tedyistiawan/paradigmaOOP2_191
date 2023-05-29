45ce8c2fad221ceeb3e86e6946f406acd1ff5df3
tedyistiawan <tedypnd07@gmail.com>
2023-05-29 18:40:59 +0700

    2

--- paradigmaOOP2_191/paradigmaOOP2_191.cpp
+++ paradigmaOOP2_191/paradigmaOOP2_191.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 using namespace std;
 
-class baseClass final {
+class baseClass {
 public:
 	virtual void perkenalan() {
 		cout << "Halo saya Function dari base class";
 	}
 };
 
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Halo saya Function dari derived Class";
+	}
+};
+
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
